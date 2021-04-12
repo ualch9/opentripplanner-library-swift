a30cf83c456a09b6fea2b9d13ad98c0a73e4c3d8
Alan Chu <ualch9@gmail.com>
2021-04-12 11:32:37 -0700

    standalone package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OpenTripPlannerClientLibrary",
-            path: "./OpenTripPlannerClientLibrary.xcframework"
+            url: "https://github.com/ualch9/opentripplanner-library-swift/releases/download/0.6/OpenTripPlannerClientLibrary.xcframework.zip",
+            checksum: "c7283cd4621f1e2a7b401254e584c5fc7ebf2dca81a25ce9e31039afd29dfd4a"
         ),
     ]
 )
